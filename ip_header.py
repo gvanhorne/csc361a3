185b069320cbd152400a5d194751937cde9d23cc
Gregory Van Horne <gvanhorne@telus.net>
2023-12-03 20:39:47 -0800

    Add method to check protocol of IP header

--- ip_header.py
+++ ip_header.py
@@ -5,30 +5,33 @@ class IPHeader:
     dst_ip = None #<type 'str'>
     ip_header_len = None #<type 'int'>
     total_len = None    #<type 'int'>
-    
+
     def __init__(self):
         self.src_ip = None
         self.dst_ip = None
         self.ip_header_len = 0
         self.total_len = 0
-    
+
     def ip_set(self,src_ip,dst_ip):
         self.src_ip = src_ip
         self.dst_ip = dst_ip
-    
+
     def header_len_set(self,length):
         self.ip_header_len = length
-    
+
     def total_len_set(self, length):
-        self.total_len = length    
-        
+        self.total_len = length
+
+    def protocol_set(self, protocol):
+        self.protocol = protocol
+
     def get_IP(self,buffer1,buffer2):
         src_addr = struct.unpack('BBBB',buffer1)
         dst_addr = struct.unpack('BBBB',buffer2)
         s_ip = str(src_addr[0])+'.'+str(src_addr[1])+'.'+str(src_addr[2])+'.'+str(src_addr[3])
         d_ip = str(dst_addr[0])+'.'+str(dst_addr[1])+'.'+str(dst_addr[2])+'.'+str(dst_addr[3])
         self.ip_set(s_ip, d_ip)
-        
+
     def get_header_len(self,value):
         result = struct.unpack('B', value)[0]
         length = (result & 15)*4
@@ -41,3 +44,7 @@ class IPHeader:
         num4 = (buffer[1]&15)
         length = num1+num2+num3+num4
         self.total_len_set(length)
+
+    def get_protocol(self, buffer):
+        protocol = struct.unpack('B', buffer)[0]
+        self.protocol_set(protocol)
