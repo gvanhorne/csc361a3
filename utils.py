bea593a7ce2a7476a7efac1ea6982034a6da1e30
Gregory Van Horne <gvanhorne@telus.net>
2023-12-03 22:47:08 -0800

    Add byte order function to utils

--- utils.py
+++ utils.py
@@ -1,4 +1,15 @@
-from packet import Packet
+def filtered_packet(packet) -> bool:
+    # add protocol 1 when ready
+    return packet.ip_header.protocol != 17 or (packet.datagram_header.src_port == 53 or packet.datagram_header.dst_port == 53)
 
-def filtered_packet(packet: Packet) -> bool:
-    return packet.ip_header.protocol != 17 and packet.ip_header.protocol != 1 and packet.datagram_header.src_port != 53 and packet.datagram_header.dst_port != 53
+def get_byte_order(magic_number):
+    if magic_number == '0xa1b2c3d4':
+        return "<"
+    elif magic_number == '0xd4c3b2a1':
+        return ">"
+    elif magic_number == '0xa1b23c4d':
+        return "<"
+    elif magic_number == '0x4d3cb2a1':
+        return ">"
+    else:
+        raise ValueError("Invalid magic number")
