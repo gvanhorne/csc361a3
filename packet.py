9c351935114035fe0e38b82d9622e22934db3013
Gregory Van Horne <gvanhorne@telus.net>
2023-12-03 20:41:29 -0800

    Build udp header from packet bytes

--- packet.py
+++ packet.py
@@ -1,5 +1,5 @@
 from ip_header import IPHeader
-from tcp_header import TCPHeader
+from udp_header import UDPHeader
 from ethernet_header import EthernetHeader
 
 class Packet():
@@ -12,24 +12,24 @@ class Packet():
     RTT_flag = False
     buffer = None
     data_bytes = 0
-    
-    
-    def __init__(self, ip_header, tcp_header, packet_bytes):
+
+
+    def __init__(self, ip_header, datagram_header, packet_bytes):
         self.ip_header = ip_header
-        self.tcp_header = tcp_header
+        self.datagram_header = datagram_header
         #self.pcap_hd_info = pcap_ph_info()
         self.timestamp = 0
         self.packet_No =0
         self.RTT_value = 0.0
         self.RTT_flag = False
         self.buffer = None
-        self.data_bytes = len(packet_bytes[14 + ip_header.ip_header_len + tcp_header.data_offset:]
+        self.data_bytes = len(packet_bytes[14 + ip_header.ip_header_len + datagram_header.data_offset:]
             .rstrip(b'\x00'))
-        
+
     def packet_No_set(self,number):
         self.packet_No = number
         #print(self.packet_No)
-        
+
     def get_RTT_value(self,p):
         rtt = p.timestamp-self.timestamp
         self.RTT_value = round(rtt,8)
@@ -38,22 +38,20 @@ class Packet():
     def from_bytes(cls, packet_bytes):
         ethernet_bytes = packet_bytes[:14]
         ethernet_header = EthernetHeader.from_bytes(ethernet_bytes)
-        
+
         ip_header = IPHeader()
         ip_header.get_header_len(packet_bytes[14:15])
         ip_header.get_total_len(packet_bytes[16:18])
         ip_header_bytes = packet_bytes[14:14+ip_header.ip_header_len]
+        protocol = ip_header.get_protocol(packet_bytes[23:24])
         ip_header.get_IP(packet_bytes[26:30], packet_bytes[30:34])
-
-        tcp_header = TCPHeader()
-        tcp_header.get_data_offset(packet_bytes[14+ip_header.ip_header_len + 12:14+ip_header.ip_header_len + 13])
-        tcp_header_bytes = packet_bytes[14+ip_header.ip_header_len:14+ip_header.ip_header_len+tcp_header.data_offset]
-        tcp_header.get_src_port(packet_bytes[14+ip_header.ip_header_len:14+ip_header.ip_header_len + 2])
-        tcp_header.get_dst_port(packet_bytes[14+ip_header.ip_header_len + 2:14+ip_header.ip_header_len + 4])
-        tcp_header.get_flags(packet_bytes[14+ip_header.ip_header_len + 13:14+ip_header.ip_header_len + 14])
-        if (tcp_header.flags["ACK"] == 1):
-            tcp_header.get_ack_num(packet_bytes[14+ip_header.ip_header_len + 8:14+ip_header.ip_header_len + 12])
-        tcp_header.get_seq_num(packet_bytes[14+ip_header.ip_header_len + 4:14+ip_header.ip_header_len + 8])
-        tcp_header.get_window_size(packet_bytes[14+ip_header.ip_header_len + 14:14+ip_header.ip_header_len + 15],
-                                    packet_bytes[14+ip_header.ip_header_len + 15:14+ip_header.ip_header_len + 16])
-        return cls(ip_header, tcp_header, packet_bytes)
+        if protocol == 17:
+            header = UDPHeader()
+            offset = 14+ip_header.ip_header_len
+            header.get_src_port(packet_bytes[offset:offset + 2])
+            header.get_dst_port(packet_bytes[offset + 2:offset + 4])
+            header.get_len(packet_bytes[offset + 4:offset + 6])
+            header.get_checksum(packet_bytes[offset + 6:offset + 8])
+        else:
+            header = None
+        return cls(ip_header, header, packet_bytes)
