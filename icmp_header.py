7a01de85d48d79a1cc6ed5eefec63e6be860ff26
Gregory Van Horne <gvanhorne@telus.net>
2023-12-04 00:49:12 -0800

    Add copy of IP header to icmp message

--- icmp_header.py
+++ icmp_header.py
@@ -5,6 +5,9 @@ class ICMPHeader:
         self.type = None
         self.code = None
         self.checksum = None
+        self.rest = None
+        self.ip_header_copy = None
+        self.udp_copy = None
 
     def set_type(self, type):
         self.type = type
@@ -14,6 +17,15 @@ class ICMPHeader:
 
     def set_checksum(self, checksum):
         self.checksum = checksum
+
+    def set_rest_of_header(self, rest):
+        self.rest = rest
+
+    def set_ip_header_copy(self, ip_header_copy):
+        self.ip_header_copy = ip_header_copy
+
+    def set_udp_copy(self, udp_copy):
+        self.udp_copy = udp_copy
 
     def get_type(self, buffer):
         type = struct.unpack('B', buffer)[0]
@@ -26,3 +38,7 @@ class ICMPHeader:
     def get_checksum(self, buffer):
         checksum = struct.unpack('>H', buffer)[0]
         self.set_checksum(checksum)
+
+    def get_rest_of_header(self, buffer):
+        rest_of_header = struct.unpack('>HH', buffer)[0]
+        self.set_rest_of_header(rest_of_header)
